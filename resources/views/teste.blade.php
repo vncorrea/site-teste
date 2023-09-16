c30394924b89257d243f8a94aefaa72c7333571c
vncorrea <vinicorreag2001@gmail.com>
2023-09-16 19:01:23 -0300

    changes

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -1,4 +1,5 @@
-<head>
-    @vite([ 'resources/sass/app.scss', 'resources/js/app.js',])
-</head>
-<div class="fs-1 fw-bolder">Oi</div>
+@extends('layout.master')
+
+@section('content')
+    <div class="fs-1 fw-bolder">Oi</div>
+@endsection
