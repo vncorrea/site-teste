9241af4bf75d1ab9b0af062432c043118556b081
vncorrea <vinicorreag2001@gmail.com>
2023-09-16 20:21:02 -0300

    changes

---
+++ resources/views/layout/views/header.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.base')
+
+<div class="pre-header">
+    <div class="bg-bs d-flex align-items-center justify-content-around flex-row ">
+        <div class="name">
+            <img src="/resources/images/ideogram%20(1).jpeg" alt="">
+            <p class="fs-2 text-white">BOOKSTAR</p>
+        </div>
+
+        <div class="cursor-pointer show menu-dropdown" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+            <i class="fas fa-user text-white"></i>
+        </div>
+    </div>
+</div>
